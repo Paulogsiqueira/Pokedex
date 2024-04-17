a10473377d393c5c76b14fe5cc9ddb4b59259c9f
Paulogsiqueira <pgustavo-siqueira@hotmail.com>
2024-04-17 17:11:06 -0300

    Commit scroll

--- Pokedex/src/App.vue
+++ Pokedex/src/App.vue
@@ -1,47 +1,15 @@
 <script setup>
 import CardList from "./components/CardList.vue";
-import { onMounted, ref, computed, reactive } from "vue";
+import { ref, computed, reactive } from "vue";
 import InputFilter from "./components/InputFilter.vue";
-import axios from "axios";
-import { useInfiniteScroll } from "@vueuse/core";
 
-let minValue = ref(0);
-let maxValue = ref(30);
+
 const listEl = ref(null);
-const usersToShow = 15;
-const fetchingData = ref(null);
 let searchValue = ref("");
 let searchType = ref("Name");
 
 const data = reactive({
   usersList: [],
-});
-
-const getUsers = async () => {
-  const users = await axios.get(
-    `https://pokeapi.co/api/v2/pokemon?limit=${maxValue.value}&offset=${minValue.value}`
-  );
-  return users.data.results;
-};
-
-const getUsersOnScroll = async () => {
-  await new Promise((res) => setTimeout(res, 2000));
-  minValue.value = maxValue.value + 1;
-  maxValue.value += 20;
-  const newUsers = await getUsers();
-  fetchingData.value = null;
-  data.usersList.push(...newUsers);
-};
-const onScrollToEnd = () => {
-  if (fetchingData.value === null) {
-    fetchingData.value = getUsersOnScroll();
-  }
-};
-
-useInfiniteScroll(listEl, async () => {
-  await getUsersOnScroll();
-},{
-  distance: 10
 });
 
 const updateSearchValue = (newValue) => {
@@ -51,11 +19,6 @@ const updateSearchValue = (newValue) => {
 const updateSearchType = (newValue) => {
   searchType.value = newValue;
 };
-
-onMounted(async () => {
-  const newUsers = await getUsers(usersToShow, 0);
-  data.usersList.push(...newUsers);
-});
 
 const pokemonsFiltered = computed(() => {
   if (data.usersList && searchValue.value) {
@@ -76,9 +39,6 @@ const pokemonsFiltered = computed(() => {
 <template>
   <div class="fullContent">
     <h1>Pokedex</h1>
-    <ul ref="listEl">
-      <li v-for="user in data.usersList" :key="user.name">{{ user.name }}</li>
-    </ul>
     <InputFilter
       v-model="searchValue"
       @updateSearchValue="updateSearchValue"
@@ -95,5 +55,7 @@ const pokemonsFiltered = computed(() => {
   justify-content: center;
   align-items: center;
   width: 100%;
+  max-height: 100vh;
 }
+
 </style>
--- Pokedex/src/components/CardList.vue
+++ Pokedex/src/components/CardList.vue
@@ -1,6 +1,43 @@
 <script setup>
-import { ref } from "vue";
 import Card from "./Card.vue";
+import { onMounted, ref, computed, reactive } from "vue";
+import axios from "axios";
+import { useInfiniteScroll } from "@vueuse/core";
+
+let minValue = ref(0);
+let maxValue = ref(0);
+const listEl = ref(null);
+const fetchingData = ref(null);
+
+const data = reactive({
+  usersList: [],
+});
+
+const getUsers = async () => {
+  const users = await axios.get(
+    `https://pokeapi.co/api/v2/pokemon?limit=${maxValue.value}&offset=${minValue.value}`
+  );
+  return users.data.results;
+};
+
+const getUsersOnScroll = async () => {
+  minValue.value = maxValue.value + 1;
+  maxValue.value += 20;
+  const newUsers = await getUsers();
+  fetchingData.value = null;
+  data.usersList.push(...newUsers);
+};
+
+useInfiniteScroll(listEl, async () => {
+  await getUsersOnScroll();
+},{
+  distance: 10
+});
+
+onMounted(async () => {
+  const newUsers = await getUsers();
+  data.usersList.push(...newUsers);
+});
 
 const props = defineProps({
   pokemonsList: Array,
@@ -9,11 +46,14 @@ const props = defineProps({
 </script>
 
 <template>
-  <div>
+  <div class="cardList-fullContent">
+    <ul ref="listEl" class="listContent">
+      <li v-for="user in data.usersList" :key="user.name">{{ user.name }}</li>
+    </ul>
     <div class="cardList">
-      <ul class="cardList-list">
+      <ul ref="listEl" class="listContent">
         <Card
-          v-for="pokemon in props.pokemonsList"
+          v-for="pokemon in props.usersList"
           :key="pokemon.name"
           :pokemon="pokemon"
         />
@@ -32,5 +72,14 @@ const props = defineProps({
 .cardList-list {
   display: flex;
   flex-wrap: wrap;
+}
+
+.listContent{
+  max-height: 100vh;
+  overflow-y: auto;
+}
+
+.cardList-fullContent{
+  border:1px solid red
 }
 </style>
